5d2b901b3f919275ead313435ed6f19733b50ff3
Chris K.Y. FUNG <8746768+chriskyfung@users.noreply.github.com>
2019-06-05 06:10:49 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# onenote macro xml parser
---
+++ onetastic_macro_xml_praser.m
@@ -0,0 +1,4 @@
+%% OneNote Macro XML Praser
+macro_xml = 'macro_xml_priority_matrix.xml';
+% regexp(s,'<Macro name="')
+xmlstruct = xml2struct(macro_xml);
